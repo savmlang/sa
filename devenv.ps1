b166cedc0e28de9f30caa6b9da55639628129f9e
Akshanabha Chakraborty <ahqsecret@gmail.com>
2026-04-16 22:37:24 +0530

    More settings

--- devenv.ps1
+++ devenv.ps1
@@ -1,8 +1,11 @@
 if ($true -ne (Test-Path "./llvm")) {
   Write-Error "LLVM is not installed - please run `./sacli` first to install it"
+  
   exit 1
 }
 
 $env:LLVM_SYS_211_PREFIX = "$(Get-Location)$([IO.Path]::DirectorySeparatorChar)llvm$([IO.Path]::DirectorySeparatorChar)bin"
 
-"✅ Your developer environment has been set up for LLVM"
+"✅ Your session has been loaded with SaVM Required Environment Variables"
+
+exit 0
