254284f77ecbbfe9880e921ac4fdd2375e0d97b2
Akshanabha Chakraborty <ahqsecret@gmail.com>
2026-04-26 21:16:11 +0530

    update

--- savm/src/acaot/native/cranelift/irgen/reg/vector.rs
+++ savm/src/acaot/native/cranelift/irgen/reg/vector.rs
@@ -12,40 +12,29 @@ pub fn abstract_insertlane(
   valadd: Value,
   idx: u8,
 ) -> Value {
-  let cliftype = builder.func.dfg.value_type(vector);
+  // let cliftype = builder.func.dfg.value_type(vector);
 
-  let single_elem_width = cliftype.lane_type().bytes();
+  // let single_elem_width = cliftype.lane_type().bytes();
 
-  // If bytes > 8, i.e. >64-bits
-  if cliftype.bytes() > 8 {
-    return builder.ins().insertlane(vector, valadd, idx);
-  }
+  // if cliftype.bytes() > 8 {
+  return builder.ins().insertlane(vector, valadd, idx);
+  // }
 
-  builder.ins().stack_store(vector, meta.regspill, 0);
+  // let canoical = canonical(cliftype);
 
-  let canoical = canonical(cliftype);
+  // let lanes = 16 / single_elem_width;
 
-  // Largen both sides
-  let vect = {
-    let val = builder.ins().stack_load(canoical, meta.regspill, 0);
-
-    let mask = ((1u64 << single_elem_width * 8) - 1) << (single_elem_width * 8 * idx as u32);
-
-    builder.ins().band_imm(val, !mask.cast_signed())
-  };
-
-  let val_shifted = {
-    let bits_shl = single_elem_width * 8 * (idx as u32);
-    let val = builder.ins().uextend(canoical, valadd);
-
-    builder.ins().ishl_imm(val, bits_shl as i64)
-  };
-
-  let vect = builder.ins().bor(vect, val_shifted);
-
-  builder.ins().stack_store(vect, meta.regspill, 0);
-
-  builder.ins().stack_load(cliftype, meta.regspill, 0)
+  // let can = builder.ins().bitcast(
+  //   canoical,
+  //   MemFlags::new().with_endianness(Endianness::Little),
+  //   vector,
+  // );
+  // let can_ext = builder.ins().uextend(I128, can);
+  // let ext = builder.ins().bitcast(
+  //   cliftype.lane_type().by(lanes).unwrap(),
+  //   MemFlags::new().with_endianness(Endianness::Little),
+  //   can_ext,
+  // );
 }
 
 pub fn abstract_extractlane(
@@ -54,52 +43,54 @@ pub fn abstract_extractlane(
   vector: Value,
   idx: u8,
 ) -> Value {
-  let cliftype = builder.func.dfg.value_type(vector);
+  // let cliftype = builder.func.dfg.value_type(vector);
 
-  let single_elem_width = cliftype.lane_type().bytes();
+  // let single_elem_width = cliftype.lane_type().bytes();
 
-  // If bytes > 8, i.e. >64-bits
-  if cliftype.bytes() > 8 {
-    return builder.ins().extractlane(vector, idx);
-  }
+  // // If bytes > 8, i.e. >64-bits
+  // if cliftype.bytes() > 8 {
+  return builder.ins().extractlane(vector, idx);
+  // }
 
-  builder.ins().stack_store(vector, meta.regspill, 0);
+  // let canoical = canonical(cliftype);
 
-  let canoical = canonical(cliftype);
+  // // Largen both sides
+  // let masked = {
+  //   let val = builder.ins().bitcast(
+  //     canoical,
+  //     MemFlags::new().with_endianness(Endianness::Little),
+  //     vector,
+  //   );
 
-  // Largen both sides
-  let masked = {
-    let val = builder.ins().stack_load(canoical, meta.regspill, 0);
+  //   let mask = ((1u64 << single_elem_width * 8) - 1) << (single_elem_width * 8 * idx as u32);
 
-    let mask = ((1u64 << single_elem_width * 8) - 1) << (single_elem_width * 8 * idx as u32);
+  //   builder.ins().band_imm(val, mask.cast_signed())
+  // };
 
-    builder.ins().band_imm(val, mask.cast_signed())
-  };
+  // let extracted = builder
+  //   .ins()
+  //   .ushr_imm(masked, (single_elem_width * 8 * (idx as u32)) as i64);
 
-  let extracted = builder
-    .ins()
-    .ushr_imm(masked, (single_elem_width * 8 * (idx as u32)) as i64);
-
-  builder.ins().ireduce(cliftype.lane_type(), extracted)
+  // builder.ins().ireduce(cliftype.lane_type(), extracted)
 }
 
-fn canonical(cliftype: Type) -> Type {
-  match cliftype.bytes() {
-    8 => {
-      // if cliftype.is_float() {
-      // F64
-      // } else {
-      I64
-      // }
-    }
-    4 => {
-      // if cliftype.is_float() {
-      //   F32
-      // } else {
-      I32
-      // }
-    }
-    2 => I16,
-    _ => INVALID,
-  }
-}
+// fn canonical(cliftype: Type) -> Type {
+//   match cliftype.bytes() {
+//     8 => {
+//       // if cliftype.is_float() {
+//       // F64
+//       // } else {
+//       I64
+//       // }
+//     }
+//     4 => {
+//       // if cliftype.is_float() {
+//       //   F32
+//       // } else {
+//       I32
+//       // }
+//     }
+//     2 => I16,
+//     _ => INVALID,
+//   }
+// }
