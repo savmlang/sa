e171f94b5514c0f32f9ffdc608e042333cf07105
Akshanabha Chakraborty <ahqsecret@gmail.com>
2026-05-14 07:22:24 +0530

    satest now should be run as a separate step

--- scripts/savm/testing.sh
+++ scripts/savm/testing.sh
@@ -3,7 +3,9 @@ export SYSROOT=$HOME/sysroot/
 echo "Running satest"
 
 sudo cp -r $(pwd)/target/$TARGET/release/ $SYSROOT/root/savm/
+sudo cp -r $(pwd)/target/$TARGET/release/libsalloc.so $SYSROOT/usr/lib/libsalloc.so
+
 sudo cp -r $(pwd)/bin/satest/tests/ $SYSROOT/root/tests/
 
 sudo chroot $SYSROOT /bin/sh -c \
-  "cd /root && chmod +x * && ./savm/satest"
+  "ldconfig && cd /root && chmod +x * && ./savm/satest"
