95a39b295f84151213a2f3d9b8a07e8e8b03f80a
Akshanabha Chakraborty <ahqsecret@gmail.com>
2026-05-31 14:59:33 +0530

    SaJIT World - use -no-rtti

--- savm/build.rs
+++ savm/build.rs
@@ -60,11 +60,23 @@ fn build_ssaupdater() {
 
   let include_llvm = llvm_config(&["--includedir"]);
 
-  Build::new()
+  let mut build = Build::new();
+
+  build
     .cpp(true)
     .std("c++20")
     .file("./ssaupdater/updater.cpp")
     .include("ssaupdater")
-    .include(include_llvm.trim())
-    .compile("ssaupdater");
+    .include(include_llvm.trim());
+
+  let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
+  let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+
+  if target_os == "linux"
+    && (target_arch == "powerpc64le" || target_arch == "powerpc64" || target_arch == "riscv64")
+  {
+    build.flag("-fno-rtti");
+  }
+
+  build.compile("ssaupdater");
 }
