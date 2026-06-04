687ecb7ba11c63c47a7cc7dbbe444db399ffdda5
Akshanabha Chakraborty <ahqsecret@gmail.com>
2026-06-04 20:42:27 +0530

    New buildscript

--- savm/build.rs
+++ savm/build.rs
@@ -69,10 +69,9 @@ fn build_ssaupdater() {
     .include("ssaupdater")
     .include(include_llvm.trim());
 
-  let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
   let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
 
-  if target_os == "linux" || target_os == "darwin"
+  if target_os == "linux"
   {
     build.flag("-fno-rtti");
   }
