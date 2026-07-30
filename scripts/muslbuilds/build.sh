0628327be668a6134839f85a2e7536221ddb81f7
Akshanabha Chakraborty <ahqsecret@gmail.com>
2026-07-30 22:25:51 +0530

    Try new MUSL build variation

---
+++ scripts/muslbuilds/build.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/sh
+
+export SYSROOT=$HOME/sysroot/
+
+# c compiler
+export CC="clang"
+export CXX="clang++"
+export AR="llvm-ar"
+
+# cc flags
+export CFLAGS="--sysroot=$SYSROOT -fuse-ld=lld"
+export CXXFLAGS="--sysroot=$SYSROOT -fuse-ld=lld"
+export BINDGEN_EXTRA_CLANG_ARGS="--sysroot=$SYSROOT -fuse-ld=lld"
+
+# libffi
+export PKG_CONFIG_SYSROOT_DIR="$SYSROOT"
+export PKG_CONFIG_PATH="$SYSROOT/usr/lib/$CTARGET/pkgconfig"
+export PKG_CONFIG_ALLOW_CROSS=1
+
+# use lld linker
+export RUSTFLAGS="-C linker=clang \
+  -C link-arg=-fuse-ld=lld \
+  -C link-arg=--target=$CTARGET \
+  -C link-arg=--sysroot=$SYSROOT \
+  -L target/debug \
+  -L target/release \
+  -L target/$TARGET/debug \
+  -L target/$TARGET/release"
+
+if [[ "$TARGET" == "armv7-unknown-linux-gnueabihf" ]]; then
+  export RUSTFLAGS="$RUSTFLAGS \
+    -C link-arg=-Wl,--allow-shlib-undefined"
+fi
+
+echo "Building C Libraries"
+
+cargo build \
+  --no-default-features \
+  -Zbuild-std=std,core,proc_macro,panic_abort \
+  --release \
+  --target $TARGET \
+  -p savmasync -p salloc
+  
+echo "Building SaVM Substrate"
+
+cargo build \
+  --workspace \
+  --no-default-features \
+  -Zbuild-std=std,core,proc_macro,panic_abort \
+  --release \
+  --target $TARGET $EXTRA \
+  --features savm/ffi_system$FEATURES $FLAGS
---
+++ scripts/muslbuilds/sysroot.sh
@@ -0,0 +1,18 @@
+sudo apt update && sudo apt install -y clang lld llvm symlinks qemu-user-static binfmt-support
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Build the SYSROOT
+sudo podman build --platform "$TARGET_PLATFORM" -t ahqrt-sysroot:latest $SCRIPT_DIR
+ahqsysroot=$(sudo podman create --platform "$TARGET_PLATFORM" ahqrt-sysroot:latest)
+sudo podman export $ahqsysroot -o $HOME/sysroot.tar.gz
+sudo podman rm $ahqsysroot
+
+# Extract sysroot
+rm -rf $HOME/sysroot || true
+mkdir $HOME/sysroot || true
+
+tar -xf $HOME/sysroot.tar.gz -C $HOME/sysroot
+
+sudo symlinks -cr $HOME/sysroot
+sudo chown -R $(whoami):$(whoami) $HOME/sysroot
